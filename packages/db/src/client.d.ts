62a788b2fd246d478694522ec99d19235979b6f4
sj8687 <shreyashjadhav59807@gmail.com>
2025-06-14 20:30:58 +0530

    test json

--- packages/db/src/client.d.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaClient } from "../generated/prisma";
-export declare const prisma: PrismaClient<import("../generated/prisma").Prisma.PrismaClientOptions, never, import("../generated/prisma/runtime/library").DefaultArgs>;
-//# sourceMappingURL=client.d.ts.map
