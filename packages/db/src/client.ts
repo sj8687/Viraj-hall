62f8234e9b796d2d03a2174daf99b8c201c0708f
sj8687 <shreyashjadhav59807@gmail.com>
2025-06-19 20:50:13 +0530

    wtf

--- packages/db/src/client.ts
+++ packages/db/src/client.ts
@@ -8,4 +8,5 @@ export const prisma =
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 
+export * from "../generated/prisma/index.js"; 
 
--- packages/db/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./client"; // ✅ export Prisma client
-export * from "../generated/prisma/index.js"; // ✅ export Prisma models/types
